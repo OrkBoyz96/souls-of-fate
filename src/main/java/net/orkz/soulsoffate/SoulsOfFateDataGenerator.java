c737b94615026062e890657234e735aa81751eb5
runni <runningwings21@gmail.com>
2026-05-15 17:15:46 -0400

    v.01

---
+++ src/main/java/net/orkz/soulsoffate/SoulsOfFateDataGenerator.java
@@ -0,0 +1,11 @@
+package net.orkz.soulsoffate;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SoulsOfFateDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/orkz/soulsoffate/SoulsOfFateclient.java
@@ -0,0 +1,10 @@
+package net.orkz.soulsoffate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulsOfFateclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
